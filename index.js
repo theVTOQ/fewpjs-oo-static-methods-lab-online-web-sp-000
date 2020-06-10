721d18b19547a69decdcfb76384fb30436d29a5b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 02:53:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z-' ]/g, "");
+  }
+
+  static titleize(string){
+    const words = string.split(' '); // .map(word => word.toLowerCase());
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const title = capitalize(words[0]); //just a starting point
+
+    words.forEach(word => {
+      let matchFound = false;
+      exceptions.forEach(exception => {
+        if(word === exception){
+            title += word;
+            matchFound = true;
+        }
+      });
+      if(!matchFound){
+        title += this.capitalize(word);
+      }
+    });
+    return title;
+  }
 }
